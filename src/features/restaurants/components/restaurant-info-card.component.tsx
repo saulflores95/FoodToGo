eb7752e92148a839b32ed07c76ef7d63e6b48803
Saul Flores <saulpolo95@gmail.com>
2022-12-19 16:49:37 -0800

    added start vector

--- src/features/restaurants/components/restaurant-info-card.component.tsx
+++ src/features/restaurants/components/restaurant-info-card.component.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
-
+import { SvgXml } from "react-native-svg";
+import star from "../../../assets/star";
 interface Props {
   name: string;
-  icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png";
+  icon: string;
   photos: string[];
-  address: "100 Some Street";
+  address: string;
   isOpenNow: true;
-  rating: 4;
+  rating: number;
   isClosedTemporarily: false;
 }
 
@@ -22,19 +23,46 @@ const RestaurantInfoCardCover = styled(Card.Cover)`
 `;
 
 const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  padding: ${(props) => props.theme.space[3]};
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Raiting = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
 const RestaurantInfoCard: React.FC<Props> = ({
   name = "Golf Club Name",
   photos,
+  address = "100 Some Street",
+  rating,
 }) => {
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
   return (
     <RestaurantCard elevation={5}>
       <RestaurantInfoCardCover key={name} source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Raiting>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Raiting>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
